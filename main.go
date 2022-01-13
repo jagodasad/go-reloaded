6acff8b9cf90d1fd5e0c6b603dc4bad1507b8c18
jagodasad <jago.sadowska@gmail.com>
2022-01-13 16:55:02 +0000

    commit

--- main.go
+++ main.go
@@ -7,14 +7,18 @@ import (
 // function to convert hex to dec
 
 func hextodec(hexi string) string {
-	//convert string (hex) to dec
+	// convert string (hex) to dec
 	num, err := strconv.ParseInt(hexi, 16, 64)
 	if err != nil {
 		panic(err)
-		//panic(err), aborts a function and returns an error value. Prints error message
+		// panic(err), aborts a function and returns an error value. Prints error message
 	}
 	dec := strconv.Itoa(int(num))
 	// convert int to string via 'Itoa'
 	return dec
-	//returning string
+	// returning string
+}
+
+func main() {
+	hextodec("")
 }
