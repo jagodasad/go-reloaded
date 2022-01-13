e4880b23b58c36a98701c3afbdebe81619e8089d
jagodasad <jago.sadowska@gmail.com>
2022-01-13 19:01:11 +0000

    commit

--- main.go
+++ main.go
@@ -43,3 +43,29 @@ func main() {
 }
 
 //Every instance of (up) converts the word placed before in the Uppercase version of it. (Ex: "Ready, set, go (up) !" -> "Ready, set, GO !")
+
+func ToUpper(s string) string {
+	characters := []rune(s)
+	for i := 0; i < len(characters); i++ {
+		currentLetter := characters[i]
+		if currentLetter >= 'a' && currentLetter <= 'z' {
+			characters[i] = characters[i] - 32
+		}
+	}
+	return string(characters)
+}
+
+// Every instance of (low) converts the word placed before in the Lowercase version of it. (Ex: "I should stop SHOUTING (low)" -> "I should stop shouting")
+
+func ToLower(s string) string {
+	characters := []rune(s)
+	for i := 0; i < len(characters); i++ {
+		currentLetter := characters[i]
+		if currentLetter >= 'A' && currentLetter <= 'Z' {
+			characters[i] = characters[i] + 32
+		}
+	}
+	return string(characters)
+}
+
+// Every instance of (cap) transforms the previous word in the capitalized version of it. (Ex: "Welcome to the Brooklyn bridge (cap)" -> "Welcome to the Brooklyn Bridge")
